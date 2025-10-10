208aca0624a50a8233315f304e5d80fd0630f236
Sender749 <jotmahal2003@gmail.com>
2025-10-10 16:55:59 +0530

    Update database.py

--- body/database.py
+++ body/database.py
@@ -5,15 +5,23 @@ client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DB)
 db = client.captions_with_chnl
 chnl_ids = db.chnl_ids
 users = db.users
+user_channels = db.user_channels 
 
+# ---------------- Caption functions ----------------
 async def addCap(chnl_id, caption):
     dets = {"chnl_id": chnl_id, "caption": caption}
     await chnl_ids.insert_one(dets)
 
-
 async def updateCap(chnl_id, caption):
     await chnl_ids.update_one({"chnl_id": chnl_id}, {"$set": {"caption": caption}})
 
+async def get_channel_caption(chnl_id: int):
+    return await chnl_ids.find_one({"chnl_id": chnl_id})
+
+async def delete_channel_caption(chnl_id: int):
+    await chnl_ids.delete_one({"chnl_id": chnl_id})
+
+# ---------------- User functions ----------------
 async def insert(user_id):
     user_det = {"_id": user_id}
     try:
@@ -31,20 +39,16 @@ async def getid():
 
 async def delete(id):
     await users.delete_one(id)
+# ---------------- Channel functions ----------------
+async def add_channel(user_id: int, channel_id: int, channel_title: str):
+    """Add channel to user channels"""
+    await users.update_one(
+        {"_id": user_id},
+        {"$addToSet": {"channels": {"channel_id": channel_id, "channel_title": channel_title}}},
+        upsert=True
+    )
 
-user_channels = db.user_channels  # NEW COLLECTION
-
-async def add_user_channel(user_id, channel_id, channel_title):
-    """Add a channel for a user if not exists"""
-    existing = await user_channels.find_one({"user_id": user_id, "channel_id": channel_id})
-    if not existing:
-        await user_channels.insert_one({
-            "user_id": user_id,
-            "channel_id": channel_id,
-            "channel_title": channel_title
-        })
-
-async def get_user_channels(user_id):
-    """Return list of channels added by a user"""
-    cursor = user_channels.find({"user_id": user_id})
-    return [doc async for doc in cursor]
+async def get_user_channels(user_id: int):
+    """Get all channels added by user"""
+    user = await users.find_one({"_id": user_id})
+    return user.get("channels", []) if user else []
