767f26c085cd6e8b0ce6184b98b166c802974086
Sender749 <jotmahal2003@gmail.com>
2025-12-27 17:17:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=200,
+            workers=50,
             plugins={"root": PLUGIN_ROOT},
             sleep_threshold=15,
         )
